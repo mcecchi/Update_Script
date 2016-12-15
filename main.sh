fcff908f088d2a6f6905aca60f6352716a7f6e51
Ximidar <Ximidar@gmail.com>
2016-12-15 01:04:27 +0000

    Added in code for AVRDUDE as well as the hex file

--- main.sh
+++ main.sh
@@ -5,19 +5,27 @@ OCTO_DIR=$HOME_DIR/OctoPrint
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 USER_PI="sudo -u pi"
 
+#AVRDUDE
+SERIAL_PATH="/dev/ttyACM0"
+flash_arduino () {
+    avrdude -p m2560 -c wiring -P $SERIAL_PATH -b 115200 -F -v -U flash:w:$1 -D
+}
 
-
-
-
-#-------------------------------main updater-----------------------------------
+kill_octoprint () {
+    service octoprint stop
+}
+#-------------------------------Kivy Update Screen-----------------------------------
 
 kivy_run () {
     $USER_PI $HOME_DIR/$VENV/bin/python $1
 }
 
+#------------------------------ Main Update----------------------------------
+kill_octoprint
+
 kivy_run $THIS_DIR/Kivy_Popup_Updater/main.py & 
 #do the update
-sleep 5 
+flash_arduino $THIS_DIR/Marlin.ino.hex
 
 
 #exit
