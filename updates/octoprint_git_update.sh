f05e886087c3b8318a6c7de26016931c6b9a07fa
Ximidar <Ximidar@gmail.com>
2016-12-15 22:42:33 +0000

    Added in the octoprint updater

---
+++ updates/octoprint_git_update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sed -i '7s%.*%\turl = https://github.com/Robo3D/OctoPrint.git%' /home/pi/OctoPrint/.git/config
+
+cd /home/pi/OctoPrint/
+git pull
+
+
+#store that the update has occured
+echo ${0##*/} >> /home/pi/.updates.txt
+echo "Update Complete!"
+exit 0
