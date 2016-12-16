d831257858c379c0aa5b7d092c7df14427cbe8f7
Ximidar <Ximidar@gmail.com>
2016-12-16 22:07:35 +0000

    another update

--- assets/erase_eeprom.py
+++ assets/erase_eeprom.py
@@ -4,21 +4,19 @@ class Erase_eeprom():
 
     def __init__(self):
         self.setup_serial()
-        self.erase_eeprom()
 
     def setup_serial(self):
         with serial.Serial() as self.ser:
             self.ser.baudrate = 250000
             self.ser.port = ('/dev/ttyACM0')  # open serial port
             self.ser.open()
+            print("Writing M502")
+            self.ser.write(b'M502')
+            print("Closing Connection")
+            self.ser.close()
+            print("Connection Closed")
+    
+           
 
-        print(self.ser.name)
-
-    def erase_eeprom(self):
-        print("Writing M502")
-        self.ser.write(b'M502')
-        print("Closing Connection")
-        self.ser.close()
-        print("Connection Closed")
 
 ee = Erase_eeprom()
