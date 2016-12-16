4027781276179d1f1ec537444d9fa4fc5d2c7e27
Victor E <victorevector@gmail.com>
2016-12-15 21:36:58 -0800

    asdfasdf

--- updates/Install_Robo_Plugins.sh
+++ updates/Install_Robo_Plugins.sh
@@ -10,46 +10,7 @@ install_plugin () {
 }
 
 #Custom Controls Plugin
-install_plugin "https://github.com/Robo3D/octoprint-customControl/archive/master.zip" &
-
-#Marlin EEPROM Editor
-install_plugin "https://github.com/Robo3D/OctoPrint-EEPROM-Marlin/archive/master.zip" &
-
-#Firmware Updater - Robo Controlled
-install_plugin "https://github.com/Robo3D/OctoPrint-FirmwareUpdater/archive/master.zip" &
-
-#persistent Mainboard connection
-install_plugin "https://github.com/Robo3D/OctoPrint-MainboardConnection/archive/master.zip" &
-
-#Netconnectd
-install_plugin "https://github.com/Robo3D/OctoPrint-Netconnectd/archive/master.zip" &
-
-#Robotheme
-install_plugin "https://github.com/Robo3D/OctoPrint-robotheme/archive/master.zip" &
-
-#Print History
-install_plugin "https://github.com/Robo3D/OctoPrint-PrintHistory/archive/master.zip" &
-
-#Printer stats
-install_plugin "https://github.com/Robo3D/OctoPrint-Stats/archive/master.zip" &
-
-#Request Spinner
-install_plugin "https://github.com/Robo3D/OctoPrint-RequestSpinner/archive/master.zip" &
-
-#ScreenSquish
-install_plugin "https://github.com/Robo3D/OctoPrint-ScreenSquish/archive/master.zip" &
-
-#kaa print event
-install_plugin "https://github.com/Robo3D/Kaa_Printevent/archive/master.zip" &
-
-#lani
-install_plugin "https://github.com/mikerybka/OctoPrint-Lani/archive/master.zip" &
-
-#Alexa
-install_plugin "https://github.com/Robo3D/Octoprint_Alexa/archive/master.zip" &
-
-#RoboLCD
-install_plugin "https://github.com/victorevector/RoboLCD/archive/master.zip" &
+$USER_PI $HOME_DIR/$VENV/bin/pip install "https://github.com/Robo3D/octoprint-customControl/archive/master.zip" "https://github.com/Robo3D/OctoPrint-EEPROM-Marlin/archive/master.zip" "https://github.com/Robo3D/OctoPrint-FirmwareUpdater/archive/master.zip" "https://github.com/Robo3D/OctoPrint-MainboardConnection/archive/master.zip" "https://github.com/Robo3D/OctoPrint-Netconnectd/archive/master.zip" "https://github.com/Robo3D/OctoPrint-robotheme/archive/master.zip" "https://github.com/Robo3D/OctoPrint-PrintHistory/archive/master.zip" "https://github.com/Robo3D/OctoPrint-Stats/archive/master.zip" "https://github.com/Robo3D/OctoPrint-RequestSpinner/archive/master.zip" "https://github.com/Robo3D/OctoPrint-ScreenSquish/archive/master.zip" "https://github.com/Robo3D/Kaa_Printevent/archive/master.zip" "https://github.com/mikerybka/OctoPrint-Lani/archive/master.zip" "https://github.com/Robo3D/Octoprint_Alexa/archive/master.zip" "https://github.com/victorevector/RoboLCD/archive/master.zip"
 
 
 #store that the update has occured
