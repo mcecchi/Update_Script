05eace89c7253dbceb72a253bcb98142b1961b8e
Victor E <victorevector@gmail.com>
2017-03-24 18:14:25 -0700

    #30

--- updates/150_robolcd.sh
+++ updates/150_robolcd.sh
@@ -14,6 +14,7 @@ install_plugin () {
 #Install the new RoboLCD Version
 install_plugin "https://github.com/victorevector/RoboLCD/archive/1.5.0.zip"
 install_plugin "https://github.com/Robo3D/OctoPrint-FirmwareUpdater/archive/0.2.1.zip"
+install_plugin "https://github.com/Robo3D/roboOctoprint/archive/1.3.2.zip"
 
 #complete the update
 echo ${0##*/} >> /home/pi/.updates.txt
