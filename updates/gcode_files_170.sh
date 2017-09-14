bdb85fe44ac0cc43af013f219d490c7ce13325dd
mcecchi <mauro.cecchi@met-informatica.it>
2017-09-14 13:32:35 +0200

    Version 1.0.2

--- updates/gcode_files_170.sh
+++ updates/gcode_files_170.sh
@@ -13,10 +13,15 @@ rm $UPLOADS/Whistle_V29D.gcode
 rm $UPLOADS/"TEST(skirt_no_raft).gcode"
 rm $UPLOADS/"TEST(with_raft).gcode"
 rm $UPLOADS/iPhone_stand.gcode
+rm $UPLOADS/3DBenchy.gcode
+rm $UPLOADS/BirdHouse.gcode
+rm $UPLOADS/C2_Bed _Level_Spiral_Test.gcode
+rm $UPLOADS/Filament_Tube_Clips.gcode
+rm $UPLOADS/Vase.gcode
 
 echo "adding new gcode files"
 
-$USER_PI cp -a $THIS_DIR/../assets/gcode_files/. $UPLOADS/
+#$USER_PI cp -a $THIS_DIR/../assets/gcode_files/. $UPLOADS/
 
 #store that the update has occured
 echo ${0##*/} >> /home/pi/.updates.txt
--- updates/gcode_scripts_150.sh
+++ updates/gcode_scripts_150.sh
@@ -3,9 +3,18 @@
 THIS_DIR=`dirname $0`
 USER_PI="sudo -u pi"
 
+#remove script files
+echo "removing old script files"
+
+rm /home/pi/.octoprint/scripts/gcode/afterPrintCancelled
+rm /home/pi/.octoprint/scripts/gcode/afterPrintDone
+rm /home/pi/.octoprint/scripts/gcode/afterPrintPaused
+rm /home/pi/.octoprint/scripts/gcode/beforePrintResumed
+rm /home/pi/.octoprint/scripts/gcode/beforePrintStarted
+
 echo "Installing gcode_cmds"
 
-$USER_PI cp -a $THIS_DIR/../assets/gcode_scripts/. /home/pi/.octoprint/scripts/gcode/
+#$USER_PI cp -a $THIS_DIR/../assets/gcode_scripts/. /home/pi/.octoprint/scripts/gcode/
 
 #store that the update has occured
 echo ${0##*/} >> /home/pi/.updates.txt
